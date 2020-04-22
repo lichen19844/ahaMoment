c06a60cf1a1ddcb4071e07e3cb88c51f26bda80c
lichen <119449234@qq.com>
2020-04-22 23:26:17 +0800

    topbar

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,79 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="top-bar">
+			<view class="top-bar-left"><image src="../../static/images/img/one.png"></image></view>
+			<view class="top-bar-center"><image src="../../static/images/index/logo.png"></image></view>
+			<view class="top-bar-right">
+				<view class="search"><image src="../../static/images/index/search.png"></image></view>
+				<view class="add"><image src="../../static/images/index/add.png"></image></view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
-<style>
-	.content {
+<style lang="scss">
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	// width: 750rpx;
+}
+
+.top-bar {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 88rpx;
+	background: $uni-bg-color;
+	// box-shadow: 0px 1px 0px 0px rgba(0, 0, 0, 0.1);
+	border-bottom: 1px solid $uni-border-color;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	.top-bar-left {
+		// float: left;
+		image {
+			padding: 10rpx $uni-spacing-col-base 0;
+			width:68rpx;
+			height:68rpx;
+			border-radius:16rpx;
+		}
+	}
+	.top-bar-center {
+		image {
+			padding-top: 16rpx;
+			width: 88rpx;
+			height: 42rpx;
+		}
+	}
+	.top-bar-right {
+		padding-top: 10rpx;
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+		justify-content: space-between;
 		align-items: center;
-		justify-content: center;
+		.search image {
+			width: 52rpx;
+			height: 52rpx;
+		}
+		.add image {
+			padding: 0 $uni-spacing-col-base;
+			width: 48rpx;
+			height: 48rpx;
+		}
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
 </style>
