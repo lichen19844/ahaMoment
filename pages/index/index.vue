64aa95af2db2012611002f36b80bb62368551c87
lichen <119449234@qq.com>
2020-04-25 18:49:00 +0800

    improve friendlist style

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -12,7 +12,23 @@
 		</view>
 		<view class="main">
 			<!-- 好友请求 -->
-			<view class="apply"></view>
+			<!-- <view class="apply"></view> -->
+			<!-- 模拟好友申请 -->
+			<view class="friends">
+				<view class="friend-list">
+					<view class="friend-list-l">
+						<text class="tip">1</text>
+						<image src="../../static/images/index/apply.png" mode=""></image>
+					</view>
+					<view class="friend-list-r">
+						<view class="top">
+							<view class="name">香果果</view>
+							<view class="time">10:36</view>
+						</view>
+						<view class="news">久仰大名，我们可以一起讨论vue和react大前端的实验呀！</view>
+					</view>
+				</view>
+			</view>
 			<!-- 聊天列表 -->
 			<view class="friends">
 				<view class="friend-list">
@@ -25,7 +41,7 @@
 							<view class="name">椰椰酱</view>
 							<view class="time">11:59</view>
 						</view>
-						<view class="content">吃过了吗？</view>
+						<view class="news">哥老关真好吃，我们什么时候再一起去吃呀？</view>
 					</view>
 				</view>
 			</view>
@@ -112,23 +128,22 @@ export default {
 	justify-content: space-between;
 	align-items: center;
 	width: 100%;
-	// height: 128rpx;	// 技巧：和图片高度一样，再设置上下padding
-	height: 96rpx;
+	// height: 128rpx;
+	// line-height: 128rpx;
+	height: 96rpx;	// 技巧：和图片高度一样，再设置上下padding
 	padding: 16rpx 0;
 	border: 1px solid;
 	.friend-list-l {
 		position: relative;
+		border: 1px solid;
 		.tip {
-			// border: 1px solid;
 			position: absolute;
 			z-index: 100;
-			// width:48px;	//自适应
-			min-width: 36rpx;
+			min-width: 36rpx; //自适应
 			height: 36rpx;
 			line-height: 36rpx;
 			text-align: center;
-			// top: 0;
-			top: -6rpx;
+			top: 0rpx;
 			left: 68rpx;
 			background: $uni-color-warning;
 			border-radius: $uni-border-radius-circle;
@@ -136,36 +151,56 @@ export default {
 			color: $uni-text-color-inverse;
 		}
 		image {
+			top: 6rpx;
 			width: 96rpx;
 			height: 96rpx;
 			border-radius: $uni-border-radius-base;
+			background-color: $uni-color-primary;
 		}
 	}
 	.friend-list-r {
 		flex: 1;
-		border: 1px solid;
+		border: 1px solid blue;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: flex-start;
 		padding-left: 32rpx;
+		width: 100%;
 		.top {
 			border: 1px solid;
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
 			align-items: center;
-			// width: 300rpx;
+			width: 100%;
+			height: 50rpx;
+			line-height: 50rpx;
 			.name {
-				height: 50rpx;
 				font-size: 36rpx;
 				font-weight: 400;
 				color: $uni-text-color;
 				line-height: 50rpx;
 			}
+			.time {
+				font-size: $uni-font-size-sm;
+				font-weight: 400;
+				color: $uni-text-color-disable;
+				line-height: 50rpx;
+				padding-bottom: 4rpx;
+			}
 		}
-		.content {
+		.news {
 			border: 1px solid red;
+			width: 100%;
+			font-size: $uni-font-size-base;
+			color: $uni-text-color-grey;
+			line-height: 40rpx;
+			// 省略号处理方法
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 1;
+			overflow: hidden;
 		}
 	}
 }
